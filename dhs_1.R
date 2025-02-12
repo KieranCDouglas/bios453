c94aa9bec9d10bedd62156c3cbc489315c88e96f
KieranCDouglas <kieran@arizona.edu>
2025-02-12 11:18:45 -0700

    prelim work
    - imported and explored data
    - ran several simple regressions and generated summary stats
    - light cleaning to make data workable
    - a few simple plots for exploration

--- dhs_1.R
+++ dhs_1.R
@@ -1,2 +1,37 @@
 ### 453 initial code 
 ### Kieran Douglas
+
+install.packages("tidyverse")
+library(tidyverse)
+
+dhs <- read_csv("")
+
+dhs_clean <- dhs %>% 
+  select(m60_1, v481, m18_1, m19_1, m17_1, v214, v457) %>% 
+  na.omit(dhs) %>% 
+  rename(
+    paradrug = m60_1,
+    insured = v481,
+    sizechild = m18_1,
+    bwkg = m19_1,
+    csect = m17_1,
+    duration = v214,
+    anemia = v457
+  ) %>% 
+  filter(
+    paradrug!=8,
+    bwkg <5000,
+    sizechild!=8
+  ) %>% 
+  mutate(
+    paradrug = as.factor(paradrug),
+  )
+
+model1 <- lm(data = dhs_clean, sizechild~paradrug+insured+bwkg+csect+duration+anemia)
+summary(model1)
+#0.00017 pvalue for association between paradrug use during pregnancy and birthweight
+#7.08e-09 pvalue for association between paradrug use during pregnancy and mothers subjective rating of childs birth size
+# seems like mothers might who use parasitic drugs during pregnancy are very likely to rate their child as being smaller at birth
+# seems like mothers who use parasitc drugs during pregnancy are much more likely to have smaller children (consistent findings)
+ggplot(data = dhs_clean, mapping = aes(x = sizechild, fill = paradrug)) +
+  geom_bar()
